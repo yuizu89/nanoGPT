43bf98a4bcf3ca0159914b143169719aac878451
yuizu89 <ykatospring@gmail.com>
2025-04-13 08:49:43 +0900

    fix argument

--- train.py
+++ train.py
@@ -148,7 +148,7 @@ if os.path.exists(meta_path):
 
 # model init
 model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=block_size,
-                  bias=bias, vocab_size=None, dropout=dropout) # start with model_args from command line
+                  bias=bias, vocab_size=None, dropout=dropout, register_token_count=register_token_count) # start with model_args from command line
 if init_from == 'scratch':
     # init a new model from scratch
     print("Initializing a new model from scratch")
