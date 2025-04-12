28c863b1a89761d88c5f6ff72297d43b8cbd8697
yuizu89 <ykatospring@gmail.com>
2025-04-12 22:51:50 +0900

    fix bug

--- model.py
+++ model.py
@@ -205,7 +205,7 @@ class GPT(nn.Module):
         if targets is not None:
             # ロス計算の際は、register token に対応する部分を除外する
             logits = self.lm_head(x)
-            logits_text = logits[:, self.register_token_count:, :]  # register token 分を除く
+            logits_text = logits[:, self.register_token_count:, :].contiguous()  # register token 分を除く
             loss = F.cross_entropy(logits_text.view(-1, logits_text.size(-1)),
                                      targets.view(-1), ignore_index=-1)
         else:
