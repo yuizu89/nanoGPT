fix loss calculation when register tokens exit

model.py
@@ -203,9 +203,10 @@ class GPT(nn.Module):
         x = self.transformer.ln_f(x)
 
         if targets is not None:
-            # if we are given some desired targets also calculate the loss
-            logits = self.lm_head(x)
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+            # ロス計算の際は、register token に対応する部分を除外する
+            logits_text = logits[:, self.register_token_count:, :]  # register token 分を除く
+            loss = F.cross_entropy(logits_text.view(-1, logits_text.size(-1)),
+                                     targets.view(-1), ignore_index=-1)
         else:
             # inference-time mini-optimization: only forward the lm_head on the very last position
             logits = self.lm_head(x[:, [-1], :]) # note: using list [-1] to preserve the time dim